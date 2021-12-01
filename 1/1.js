505302822ad85cbef3df9e1f1e30a8f793c17a7a
Alex Ferrari <alex.ferrari@nn.nl>
2021-12-01 14:00:15 +0100

    first commit

---
+++ 1/1.js
@@ -0,0 +1,46 @@
+﻿const fs = require("fs");
+
+function solvePart1(data) {
+  let count = 0;
+
+  for (let i = 1; i < data.length; i++) {
+    if (data[i] > data[i - 1]) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+function solvePart2(data) {
+  const k = 3;
+  let currSum = 0;
+  let prevSum = 0;
+  let count = 0;
+
+  for (let i = 0; i < k; i++) {
+    prevSum += data[i];
+  }
+
+  for (let i = 0; i < data.length - k; i++) {
+    currSum = prevSum - data[i] + data[i + k];
+    if (currSum > prevSum) count++;
+    prevSum = currSum;
+  }
+  return count;
+}
+
+fs.readFile("1_input.txt", "utf-8", (err, rawData) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const data = rawData
+    .trim()
+    .split("\n")
+    .map((el) => parseInt(el));
+
+  console.log(`Part 1: ${solvePart1(data)}`);
+  console.log(`Part 2: ${solvePart2(data)}`);
+});
---
+++ 1/1.py
@@ -0,0 +1,33 @@
+﻿with open("1_input.txt") as file:
+    data = [int(i) for i in file.readlines()]
+
+
+"""
+Part 1
+"""
+count = 0
+
+for i, val in enumerate(data, start=1):
+    if val > data[i - 2]:
+        count += 1
+
+print(f"Part 1: {count}")
+
+
+"""
+Part 2
+"""
+count = 0
+k = 3
+
+# calculate the sum of the first window
+prev_sum = sum(data[:k])
+curr_sum = 0
+
+for i in range(len(data) - k):
+    curr_sum = prev_sum - data[i] + data[i + k]
+    if curr_sum > prev_sum:
+        count += 1
+    prev_sum = curr_sum
+
+print(f"Part 2: {count}")
