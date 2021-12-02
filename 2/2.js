59ac1b6f00a7845a8aab6d7dbfe5ee541420e2e6
Alex Ferrari <alex.ferrari@nn.nl>
2021-12-02 09:08:47 +0100

    first gold star implementation

---
+++ 2/2.js
@@ -0,0 +1,38 @@
+﻿const fs = require("fs");
+
+function getData(file) {
+  let rawData = fs.readFileSync(file, { encoding: "utf-8" });
+  let data = rawData.trim().split("\n");
+  data = data.map((el) => {
+    tuple = el.split(" ");
+    return [tuple[0].toLowerCase(), parseInt(tuple[1])];
+  });
+  return data;
+}
+
+function calculatePosition(data) {
+  let x = (depth = 0);
+  for (const tuple of data) {
+    switch (tuple[0]) {
+      case "forward":
+        x += tuple[1];
+        break;
+      case "down":
+        depth += tuple[1];
+        break;
+      case "up":
+        depth -= tuple[1];
+      default:
+        break;
+    }
+  }
+  return x * depth;
+}
+
+function main() {
+  const data = getData("input.txt");
+  const res1 = calculatePosition(data);
+  console.log(res1);
+}
+
+main();
