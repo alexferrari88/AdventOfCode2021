9436c2eddf83c03846232d383bceb5da40181feb
Alex Ferrari <alex.ferrari@nn.nl>
2021-12-05 12:53:29 +0100

    refactor

--- 5/5.py
+++ 5/5.py
@@ -15,11 +15,7 @@ def get_data(filename):
     return (start_xy, end_xy)
 
 
-def solve_part_1(start_xy, end_xy):
-    if len(start_xy) != len(end_xy):
-        raise ValueError
-    n = len(start_xy)
-
+def generate_empty_matrix(start_xy, end_xy):
     start_maxtrix = np.array(start_xy, dtype=np.int32)
     end_maxtrix = np.array(end_xy, dtype=np.int32)
 
@@ -28,7 +24,28 @@ def solve_part_1(start_xy, end_xy):
     max_x = max([max_start_x, max_end_x])
     max_y = max([max_start_y, max_end_y])
 
-    final_matrix = np.zeros((max_y + 1, max_x + 1), dtype=np.int32)
+    return np.zeros((max_y + 1, max_x + 1), dtype=np.int32)
+
+
+def get_line_params(start_x, start_y, end_x, end_y):
+    if (start_y - start_x) == (end_y - end_x):
+        m = 1
+        c = start_y - start_x
+    elif (start_y + start_x) == (end_y + end_x):
+        m = -1
+        c = start_y + start_x
+
+    return (m, c)
+
+
+def solve_part_1(start_xy, end_xy):
+    if len(start_xy) != len(end_xy):
+        raise ValueError
+    n = len(start_xy)
+
+    start_maxtrix = np.array(start_xy, dtype=np.int32)
+    end_maxtrix = np.array(end_xy, dtype=np.int32)
+    final_matrix = generate_empty_matrix(start_xy, end_xy)
 
     for i in range(n):
         start_x = start_maxtrix[i][0]
@@ -63,13 +80,9 @@ def solve_part_2(start_xy, end_xy):
 
     start_maxtrix = np.array(start_xy, dtype=np.int32)
     end_maxtrix = np.array(end_xy, dtype=np.int32)
+    final_matrix = generate_empty_matrix(start_xy, end_xy)
 
-    max_start_x, max_start_y = start_maxtrix.max(axis=0)
-    max_end_x, max_end_y = end_maxtrix.max(axis=0)
-    max_x = max([max_start_x, max_end_x])
-    max_y = max([max_start_y, max_end_y])
-
-    final_matrix = np.zeros((max_y + 1, max_x + 1), dtype=np.int32)
+    max_y = np.shape(final_matrix)[0]
 
     for i in range(n):
         start_x = start_maxtrix[i][0]
@@ -86,15 +99,10 @@ def solve_part_2(start_xy, end_xy):
             xs = [n for n in range(min([start_x, end_x]), max([start_x, end_x]) + 1)]
             for x in xs:
                 final_matrix[y, x] += 1
-        else:
-            if (start_y - start_x) == (end_y - end_x):
-                m = 1
-                c = start_y - start_x
-            elif (start_y + start_x) == (end_y + end_x):
-                m = -1
-                c = start_y + start_x
-            else:
-                continue
+        elif (start_y - start_x) == (end_y - end_x) or (start_y + start_x) == (
+            end_y + end_x
+        ):
+            m, c = get_line_params(start_x, start_y, end_x, end_y)
             xs = [n for n in range(min([start_x, end_x]), max([start_x, end_x]) + 1)]
             for x in xs:
                 y = m * x + c
