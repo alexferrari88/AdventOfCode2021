2986aadf85db44c144478a4bcb7ae988ad5a977a
Alex Ferrari <alex.ferrari@nn.nl>
2021-12-07 07:15:41 +0100

    2nd gold star

--- 7/7.py
+++ 7/7.py
@@ -1,4 +1,5 @@
-﻿from statistics import median
+﻿import math
+from statistics import mean, median
 
 
 def get_data(filename):
@@ -10,10 +11,17 @@ def get_data(filename):
 
 def solve_part_1(data):
     num_at_center = int(median(sorted(data)))
-    print(num_at_center)
     return sum(abs(i - num_at_center) for i in data)
+
+
+def solve_part_2(data):
+    most_common = math.floor(mean(data))
+    print(most_common)
+    return sum(abs(p - most_common) + sum(range(abs(p - most_common))) for p in data)
 
 
 data = get_data("input.txt")
 res1 = solve_part_1(data)
 print(f"Res 1: {res1}")
+res2 = solve_part_2(data)
+print(f"Res 2: {res2}")
---
+++ utils/utils.py
@@ -0,0 +1,28 @@
+﻿import json
+import os
+import requests
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
+
+BASE_PATH = Path(__file__).parent
+SETTINGS_FILE_PATH = (BASE_PATH / "../settings.json").resolve()
+AOC_SESSION = os.environ.get("AOC_SESSION")
+
+with open(SETTINGS_FILE_PATH) as f:
+    settings = json.load(f)
+
+
+def download_input(day_num: int):
+    base_url = settings["base_url"]
+    year = settings["year"]
+    res = requests.get(
+        f"{base_url}{year}/day/{day_num}/input", cookies={"session": AOC_SESSION}
+    )
+    res.raise_for_status()
+    with open("input.txt", "w") as f:
+        f.write(res.content)
+
+
+download_input(1)
