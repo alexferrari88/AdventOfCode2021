c4ce191975d4fb9957ed5d72f7a33aae6783eec3
Alex Ferrari <alex.ferrari@nn.nl>
2021-12-07 07:35:29 +0100

    added template

---
+++ template.py
@@ -0,0 +1,20 @@
+﻿def get_data(filename):
+    with open(filename) as f:
+        data = [int(i) for i in f.read().strip().split(",")]
+
+    return data
+
+
+def solve_part_1(data):
+    ...
+
+
+def solve_part_2(data):
+    ...
+
+
+data = get_data("input.txt")
+res1 = solve_part_1(data)
+print(f"Res 1: {res1}")
+res2 = solve_part_2(data)
+print(f"Res 2: {res2}")
